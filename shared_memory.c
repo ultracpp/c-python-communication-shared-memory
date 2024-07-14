226d8e0dbccaea595d296dd5a2cd189d3a467752
ultracpp <ultracpp@naver.com>
2024-07-14 15:00:35 +0900

    Update shared_memory.c

--- shared_memory.c
+++ shared_memory.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
 void shared_memory_init(shared_memory* shm)
 {
@@ -24,9 +23,21 @@ void shared_memory_init(shared_memory* shm)
         exit(1);
     }
 
-    shm->hEvent = CreateEvent(NULL, FALSE, FALSE, SEM_NAME);
+    /*shm->hSemRead = CreateSemaphore(NULL, 1, 1, SEM_READ_NAME);
+    shm->hSemWrite = CreateSemaphore(NULL, 0, 1, SEM_WRITE_NAME);
 
-    if (shm->hEvent == NULL)
+    if (shm->hSemRead == NULL || shm->hSemWrite == NULL)
+    {
+        printf("CreateSemaphore error: %d\n", GetLastError());
+        UnmapViewOfFile(shm->pBuf);
+        CloseHandle(shm->hMapFile);
+        exit(1);
+    }*/
+
+    shm->hSemRead = CreateEvent(NULL, FALSE, TRUE, SEM_READ_NAME);
+    shm->hSemWrite = CreateEvent(NULL, FALSE, FALSE, SEM_WRITE_NAME);
+
+    if (shm->hSemRead == NULL || shm->hSemWrite == NULL)
     {
         printf("CreateEvent error: %d\n", GetLastError());
         UnmapViewOfFile(shm->pBuf);
@@ -58,9 +69,10 @@ void shared_memory_init(shared_memory* shm)
         exit(1);
     }
 
-    shm->sem = sem_open(SEM_NAME, O_CREAT, 0644, 0);
+    shm->sem_read = sem_open(SEM_READ_NAME, O_CREAT, 0644, 1);
+    shm->sem_write = sem_open(SEM_WRITE_NAME, O_CREAT, 0644, 0);
 
-    if (shm->sem == SEM_FAILED)
+    if (shm->sem_read == SEM_FAILED || shm->sem_write == SEM_FAILED)
     {
         perror("sem_open");
         munmap(shm->shm_base, SHM_SIZE);
@@ -70,39 +82,74 @@ void shared_memory_init(shared_memory* shm)
 #endif
 }
 
-void shared_memory_write(shared_memory* shm, const char* message)
-{
-#ifdef _WIN32
-    strncpy((char*)shm->pBuf, message, SHM_SIZE);
-    SetEvent(shm->hEvent);
-#else
-    strncpy(shm->shm_base, message, SHM_SIZE);
-    sem_post(shm->sem);
-#endif
-}
-
 void shared_memory_read(shared_memory* shm, char* buffer)
 {
 #ifdef _WIN32
-    WaitForSingleObject(shm->hEvent, INFINITE);
+    /*DWORD dwWaitResult = WaitForSingleObject(shm->hSemWrite, INFINITE);
+    if (dwWaitResult != WAIT_OBJECT_0)
+    {
+        printf("WaitForSingleObject error: %d\n", GetLastError());
+        return;
+    }
+
     strncpy(buffer, (char*)shm->pBuf, SHM_SIZE);
+
+    if (!ReleaseSemaphore(shm->hSemRead, 1, NULL))
+    {
+        printf("ReleaseSemaphore error: %d\n", GetLastError());
+    }*/
+
+    WaitForSingleObject(shm->hSemWrite, INFINITE);
+    strncpy(buffer, (char*)shm->pBuf, SHM_SIZE);
+    SetEvent(shm->hSemRead);
 #else
-    sem_wait(shm->sem);
+    sem_wait(shm->sem_write);
     strncpy(buffer, shm->shm_base, SHM_SIZE);
+    sem_post(shm->sem_read);
+#endif
+}
+
+void shared_memory_write(shared_memory* shm, const char* message)
+{
+#ifdef _WIN32
+    /*DWORD dwWaitResult = WaitForSingleObject(shm->hSemRead, INFINITE);
+    if (dwWaitResult != WAIT_OBJECT_0)
+    {
+        printf("WaitForSingleObject error: %d\n", GetLastError());
+        return;
+    }
+
+    strncpy((char*)shm->pBuf, message, SHM_SIZE);
+
+    if (!ReleaseSemaphore(shm->hSemWrite, 1, NULL))
+    {
+        printf("ReleaseSemaphore error: %d\n", GetLastError());
+    }*/
+
+    WaitForSingleObject(shm->hSemRead, INFINITE);
+    strncpy((char*)shm->pBuf, message, SHM_SIZE);
+    SetEvent(shm->hSemWrite);
+#else
+    sem_wait(shm->sem_read);
+    strncpy(shm->shm_base, message, SHM_SIZE);
+    sem_post(shm->sem_write);
 #endif
 }
 
 void shared_memory_release(shared_memory* shm)
 {
 #ifdef _WIN32
+    CloseHandle(shm->hSemRead);
+    CloseHandle(shm->hSemWrite);
     UnmapViewOfFile(shm->pBuf);
     CloseHandle(shm->hMapFile);
-    CloseHandle(shm->hEvent);
 #else
     munmap(shm->shm_base, SHM_SIZE);
     close(shm->shm_fd);
-    sem_close(shm->sem);
-    sem_unlink(SEM_NAME);
+    sem_close(shm->sem_read);
+    sem_close(shm->sem_write);
+    sem_unlink(SEM_READ_NAME);
+    sem_unlink(SEM_WRITE_NAME);
     shm_unlink(SHM_NAME);
 #endif
 }
